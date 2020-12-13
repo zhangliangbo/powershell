cad928532a25eae3b912854e34315e86282b71f8
zhangliangbo <2472110501@qq.com>
2020-12-13 17:13:48 +0800

    生成sha的分支

--- CivicGit/CivicGit.psm1
+++ CivicGit/CivicGit.psm1
@@ -1,12 +1,17 @@
 function New-ShaBranch
 {
+    [CmdletBinding()]
     param(
-        $Push
+        [ValidateNotNullOrEmpty()]
+        [Boolean]$Push = $true
     )
     $branch = Invoke-Command -ScriptBlock { git symbolic-ref --short head }
     $sha1 = Invoke-Command -ScriptBlock { git rev-parse --short head }
     $newName = $branch + '-' + $sha1
-    Invoke-Command -ScriptBlock { git branch $newName }
-    Invoke-Command -ScriptBlock { git push --set-upstream origin $newName }
+    Invoke-Command -ScriptBlock { git branch $newName } -ErrorAction Stop
+    if ($Push)
+    {
+        Invoke-Command -ScriptBlock { git push --set-upstream origin $newName } -ErrorAction Stop
+    }
     Write-Host $newName
 }
